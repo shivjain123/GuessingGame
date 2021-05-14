c9d78d9ccf5757f0ca17541f33eae2cc111b725b
Shiv Jain <70323345+shivjain123@users.noreply.github.com>
2021-05-14 14:17:00 +0530

    Update guessingGame.py

--- guessingGame.py
+++ guessingGame.py
@@ -5,12 +5,13 @@ guess = input("Please enter your guess")
 
 chances = 5
 
-while chances < 5 and > 1:
+while chances < 5 and > 1 :
     if guess == number:
         print("CONGRATULATIONS YOU WON !!!")
-    else :
+    else:
         print("You were close. " + "Guess a number higher than" + number - 3)
 break
 
 while chances < 1:
-    print("You Lose  The number is " + number)
+    print("You Lose, " + "The number is " + number)
+break
